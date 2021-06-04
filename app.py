de627f77f789f23f4f829b8a6506b76af363bf12
Thalia Stemitsiotis <thalia.stemitsiotis@gmail.com>
2021-06-04 18:37:30 +0100

    done

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import datetime
 import requests
-
+import pandas as pd
+import numpy as np
+#from geopy.geocoders import Nominatim
 
 '''
 # NY Taxi Prediction App
@@ -11,6 +13,10 @@ d = st.date_input(
     "Enter date: yyyy/mm/dd")
 
 t = st.time_input('Time:')
+
+pickup_location = st.text_input('Enter your pickup adress here', '')
+dropoff_location = st.text_input('Enter your dropoff adress here', '')
+
 p_longitude = st.number_input('Pickup longitude')
 p_latitude = st.number_input('Pickup latitude')
 d_longitude = st.number_input('Dropoff longitude')
@@ -27,6 +33,10 @@ params = {
     'dropoff_longitude':d_longitude,
     'dropoff_latitude':d_latitude,
     'passenger_count':int(p_count)}
+
+map_data = pd.DataFrame(
+    columns=['lat', 'lon'])
+st.map(map_data)
 
 if st.button('Click here for taxi fare price'):
     response = requests.get(url, params).json()
